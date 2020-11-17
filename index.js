afd519484afeda28e5021a289713c0dfd38678f8
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-17 23:26:02 +0100

    Route parameters to fetch data per id

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ app.get('/api/listing', (req, res) => {
 	res.send(homes);
 });
 
+// :id for dynamic value
+app.get('/api/listing/:id', (req, res) => {
+	// logic to check if the id exists in the array with homes.find()
+	// we make use of parseInt to convert req.params.id to interger
+	const home = homes.find((home) => home.id === parseInt(req.params.id)); // req.params.id we can return the id that we pass to the route
+
+	// check if home is false
+	if (!home) {
+		// send a status 404 with text ...
+		res.status(404).send('The home with given id cannot be found.');
+	}
+	// if home exists we send home
+	res.send(home);
+});
+
 // we can have access to the env variable
 // if theres not port in the .env it will be passed 3000
 const port = process.env.PORT || 3000;
