c146c4c38c1f3c11960f219b0dde02c7e8ac4dd7
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-18 01:08:37 +0100

    Connecting to MongoDB using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// mongoDB login
+// master-admin
+// QDs6Uy1s2E8XGtWz
+
 // require the express package
 const express = require('express');
+
+// import mongoose
+const mongoose = require('mongoose');
 
 // init express package
 const app = express();
@@ -10,116 +17,17 @@ app.use(express.json());
 // load env variable in the index.js
 require('dotenv').config();
 
-const homes = [
-	{
-		id: 1,
-		homeType: 'Apartment',
-		description: 'some dummy description',
-		role: 'admin',
-	},
-	{
-		id: 2,
-		homeType: 'Flat',
-		description: 'some dummy edited description',
-		role: 'user',
-	},
-];
-
-// listen some request (get - post - put - delete)
-// .get() function -> 1st parm: route 2nd param: function with request and response
-app.get('/', (req, res) => {
-	res.send('Welcome to express!'); // send a response to the user
-});
-
-// when user make request to /api/listing
-// we send an array of objects
-app.get('/api/listing', (req, res) => {
-	res.send(homes);
-});
-
-// :id for dynamic value
-app.get('/api/listing/:id', (req, res) => {
-	// logic to check if the id exists in the array with homes.find()
-	// we make use of parseInt to convert req.params.id to interger
-	const home = homes.find((home) => home.id === parseInt(req.params.id)); // req.params.id we can return the id that we pass to the route
-
-	// check if home is false
-	if (!home) {
-		// send a status 404 with text ...
-		res.status(404).send('The home with given id cannot be found.');
-	}
-	// if home exists we send home
-	res.send(home);
-});
-
-// post is using to get new resource
-app.post('/api/listing', (req, res) => {
-	// validation check if type or description is not provided
-	if (!req.body.homeType || !req.body.description) {
-		// if its not provided we send an error msg using res.send()
-		return res.status(400).send('Title and description is required.');
-	}
-
-	// create object with the property from homes array.
-	const home = {
-		id: homes.length + 1, // dynamic id
-		homeType: req.body.homeType, // request the data from body
-		description: req.body.description,
-		role: 'user',
-	};
-
-	// add home object we created to the array of homes
-	homes.push(home);
-	// return the data we just created
-	res.send(home);
-});
-
-// put is using to edit
-// first we need to find the id we want to edit
-// then we need pass the new req.body.homeType
-app.put('/api/listing/:id', (req, res) => {
-	// logic to check if the id exists in the array with homes.find()
-	// we make use of parseInt to convert req.params.id to interger
-	const home = homes.find((home) => home.id === parseInt(req.params.id)); // req.params.id we can return the id that we pass to the route
-
-	// check if home is false
-	if (!home) {
-		// send a status 404 with text ...
-		return res.status(404).send('The home with given id cannot be found.');
-	}
-
-	// overwrite properties
-	home.homeType = req.body.homeType;
-	home.description = req.body.description;
-
-	// send back edited object
-	res.send(home);
-});
-
-// delete request
-// first we need to find the id we want to delete
-app.delete('/api/listing/:id', (req, res) => {
-	// logic to check if the id exists in the array with homes.find()
-	// we make use of parseInt to convert req.params.id to interger
-	const home = homes.find((home) => home.id === parseInt(req.params.id)); // req.params.id we can return the id that we pass to the route
-
-	// check if home is false
-	if (!home) {
-		// send a status 404 with text ...
-		return res.status(404).send('The home with given id cannot be found.');
-	}
-
-	// indexOf() we return the item we are looking for.
-	const index = homes.indexOf(home);
-	homes.splice(index, 1); //with splice() we can remove elements from Array. Index is here the item we want to remove.
-
-	// send back the item we just deleted
-	res.send(home);
-});
-
 // we can have access to the env variable
 // if theres not port in the .env it will be passed 3000
 const port = process.env.PORT || 3000;
 
-// make request on port from env var
-app.listen(port, () => console.log(`Server is running on port ${port}.`));
+mongoose
+	.connect(
+		'mongodb+srv://master-admin:QDs6Uy1s2E8XGtWz@cluster0.c6occ.mongodb.net/home-listing-database?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then((result) => {
+		// make request on port from env var
+		app.listen(port, () => console.log(`Server is running on port ${port}.`));
+	})
+	.catch((err) => console.log(err));
