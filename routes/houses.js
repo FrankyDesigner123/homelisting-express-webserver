ace170b52bafa3791b6bc429c679402c8e32d625
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-18 02:06:56 +0100

    Adding validation

--- routes/houses.js
+++ routes/houses.js
@@ -1,14 +1,39 @@
 // import express package
 const express = require('express');
 
+// import some function from express-validator
+const { check, validationResult } = require('express-validator');
+
 // import House model
 const House = require('../models/House');
 
 // init the router
 const router = express.Router();
 
+// validation
+const validate = [
+	check('title')
+		.isLength({ min: 3, max: 50 })
+		.withMessage('Title should be between 3 to 50 characters.'),
+	check('description')
+		.isLength({ min: 10, max: 200 })
+		.withMessage('Description should be between 10 to 200 characters.'),
+	check('address')
+		.isLength({ min: 10, max: 100 })
+		.withMessage('Address should be between 10 to 100 characters.'),
+	check('price').isNumeric().withMessage('Price should be a number.'),
+];
+
 // create House Listing Data
-router.post('/', (req, res) => {
+// pass in validation as second parameter
+router.post('/', validate, (req, res) => {
+	// check the validation
+	const errors = validationResult(req);
+
+	// check errors
+	if (!errors.isEmpty()) {
+		return res.status(422).send({ errors: errors.array() });
+	}
 	// we create an instance of the Class House
 	const house = new House({
 		title: req.body.title,
